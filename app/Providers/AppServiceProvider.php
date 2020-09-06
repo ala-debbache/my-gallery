7c0c98141dfb327121ebaabbe39e7785620e5047
ala-debbache <65853803+ala-debbache@users.noreply.github.com>
2020-09-06 01:42:59 +0100

    gallery 1.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ('APP_ENV' !== 'local') {
+            URL::forceScheme('https');
+        }
         ini_set("memory_limit", "100M");
         ini_set('post_max_size', '50M');
         ini_set('upload_max_filesize', '50M');
